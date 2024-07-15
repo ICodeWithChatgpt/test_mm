a0afbb305f1790f2e501b1c81b3caac56d6860d5
Mario Rubio <mario.rubio-calvo@brandung.de>
2024-07-15 13:47:40 +0200

    Small change

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -3,7 +3,7 @@ import "../../styles/index.css"; // Corrected import path
 
 const Test = () => (
     <div className="test-container">
-        <h1 className="test-title">Hello Worlds</h1>
+        <h1 className="test-title">Hello World</h1>
         <p className="test-description">Welcome to your new Gatsby site.</p>
     </div>
 );
