a4639d9331331ef9bb1c03474b99d6f747cfa62e
Mario Rubio <mario.rubio-calvo@brandung.de>
2024-07-15 12:23:01 +0200

    Initial set of files

---
+++ src/pages/test/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../styles/index.css"; // Corrected import path
+
+const Test = () => (
+    <div className="test-container">
+        <h1 className="test-title">Hello World</h1>
+        <p className="test-description">Welcome to your new Gatsby site.</p>
+    </div>
+);
+
+export default Test;
